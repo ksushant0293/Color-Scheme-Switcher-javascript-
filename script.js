db7c5ae884e2e1539055a3c7a9ced7818bd89fd4
sushant kumar <ksushant0293@gmail.com>
2024-03-16 02:03:54 +0530

    added event listner to change color

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const rainbow = document.querySelectorAll(".rainbox-col");
-console.log(rainbow);
+// console.log(rainbow);
+const body = document.querySelector("body");
 
-rainbow.forEach(()=> {
-    
-})
+rainbow.forEach((button) => {
+    // console.log(button);
+    button.addEventListener("click", function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.style.backgroundColor === "red"){
+            body.style.backgroundColor = e.target.style.backgroundColor;
+        }
+        if(e.target.style.backgroundColor === "orange"){
+            body.style.backgroundColor = e.target.style.backgroundColor;
+        }
+        if(e.target.style.backgroundColor === "yellow"){
+            body.style.backgroundColor = e.target.style.backgroundColor;
+        }
+        if(e.target.style.backgroundColor === "green"){
+            body.style.backgroundColor = e.target.style.backgroundColor;
+        }
+        if(e.target.style.backgroundColor === "blue"){
+            body.style.backgroundColor = e.target.style.backgroundColor;
+        }
+        if(e.target.style.backgroundColor === "indigo"){
+            body.style.backgroundColor = e.target.style.backgroundColor;
+        }
+        if(e.target.style.backgroundColor === "violet"){
+            body.style.backgroundColor = e.target.style.backgroundColor;
+        }
+    });
+});
